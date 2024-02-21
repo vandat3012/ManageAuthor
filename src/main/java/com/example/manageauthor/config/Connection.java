c71c408a6f52638f174fe5b71244f7f45a9d63b1
vandat3012 <luongvandat301220@gmail.com>
2024-02-21 10:06:47 +0700

    access database

--- src/main/java/com/example/manageauthor/config/Connection.java
+++ src/main/java/com/example/manageauthor/config/Connection.java
@@ -1,8 +1,26 @@
 package com.example.manageauthor.config;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
 private static Connection connection;
 
-public Connection() {
+private Connection() {
+    }
+public static  Connection getConnection() {
+if (connection == null) {
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/manage_post",
+                "root",
+                "123456");
+    } catch (ClassNotFoundException e) {
+        throw new RuntimeException(e);
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+return connection;
 }
 }
---
+++ src/main/webapp/list.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 21/02/2024
+  Time: 9:55 SA
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List</title>
+</head>
+<body>
+
+</body>
+</html>
